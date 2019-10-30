8f990e4b3e131bf3912781515abf6be0d8411355
jschweizer78 <jschweizer78@gmail.com>
2019-10-30 15:52:45 -0700

    fixed marshiling of users

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/jschweizer78/fusion-ng
 require (
 	github.com/asdine/storm/v3 v3.0.0
 	github.com/leaanthony/mewn v0.10.7
+	github.com/mitchellh/mapstructure v1.1.2
 	github.com/spf13/viper v1.4.0
 	github.com/wailsapp/wails v0.18.5
 )
--- pkg/service/srvUsers.go
+++ pkg/service/srvUsers.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/asdine/storm/v3"
 	"github.com/jschweizer78/fusion-ng/pkg/service/model"
+	"github.com/mitchellh/mapstructure"
 )
 
 // SrvUsers is a service for users
@@ -24,7 +25,7 @@ func NewSrvUsers(db storm.Node) *SrvUsers {
 // GetAll gets all users from storm DB
 func (su *SrvUsers) GetAll() ([]*model.User, error) {
 	var users []*model.User
-	err := su.DB.All(&users)
+	err := su.DB.All(users)
 	if err != nil {
 		return nil, fmt.Errorf("coul not find users: %v", err)
 	}
@@ -38,12 +39,12 @@ func (su *SrvUsers) Name() string {
 
 // GetOne gets one user from storm DB by email
 func (su *SrvUsers) GetOne(id string) (*model.User, error) {
-	var user *model.User
-	err := su.DB.One("Email", id, user)
+	var user model.User
+	err := su.DB.One("Email", id, &user)
 	if err != nil {
 		return nil, fmt.Errorf("coul not find user %s: %v", id, err)
 	}
-	return user, nil
+	return &user, nil
 }
 
 // GetLimit gets users from storm DB by email
@@ -57,8 +58,14 @@ func (su *SrvUsers) GetLimit(field, filter string, skip, limit int) ([]*model.Us
 }
 
 // CreateOne creats one user, must have email
-func (su *SrvUsers) CreateOne(user *model.User) (string, error) {
-	err := su.DB.Save(user)
+func (su *SrvUsers) CreateOne(obj map[string]interface{}) (string, error) {
+	var user *model.User
+	err := mapstructure.Decode(obj, &user)
+	if err != nil {
+		return "", fmt.Errorf("coul not unmarshal user: %v", err)
+	}
+
+	err = su.DB.Save(user)
 	if err != nil {
 		return "", fmt.Errorf("coul not create user %s: %v", user.Email, err)
 	}
